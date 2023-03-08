977285b2d5d90e38d7fb5ed654e11da5ae3e5cf8
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-08 20:56:01 -0300

    Initial commit

---
+++ Lista3.py
@@ -0,0 +1,11 @@
+#Entrada de dados
+
+conta = float(input("Digite o valor da conta: "))
+
+#Processamento
+
+res = (conta * 0.20)
+
+#Saida
+
+print(f"Kwh da sua conta eh: {res:.2f}")
---
+++ Lista3dolar.py
@@ -0,0 +1,10 @@
+#Entrada de dados
+
+dolar = float(input("Digite o valor em dolar: "))
+
+#Processamento
+
+res = (dolar * 5.15)
+
+#Saida
+print(f"A conversao para real fica: {res:.2f}")
